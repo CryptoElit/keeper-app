2e19248259dae4308093cfbb8d5fab0ec507182a
CryptoElit <heinzgericke@outlook.com>
2022-07-03 17:39:11 +0200

    added Note component + updated styling on Header.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 
 import React from "react";
 import Header from "./Header";
+import Note from "./Note";
 
 
 function App() {
     return(
         <div>
             <Header />
+            <Note />
         </div>
     );
 }
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return(
+        <div className="note">
+            <h1 className="note h1">Add Task...</h1>
+            <p className="note p">Add description...</p>
+        </div>
+    );
+}
+
+export default Note;
